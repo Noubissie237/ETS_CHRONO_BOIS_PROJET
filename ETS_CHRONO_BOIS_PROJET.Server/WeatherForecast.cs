43f250fb15c6b5146f0b853754a79af17c2b7d7e
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-06-04 01:43:23 +0200

    Ajoutez des fichiers projet.

---
+++ ETS_CHRONO_BOIS_PROJET.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ETS_CHRONO_BOIS_PROJET.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
